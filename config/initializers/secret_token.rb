60e90f72194f6aa5774cce0d5f4633aef4e30b5d
Byron Austin <byronaustin007@gmail.com>
2012-05-09 13:26:27 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boyosplace::Application.config.secret_token = '3b4796e77ef3920710dc993f9b7fbfe3963b1ba20b767b333bc393350cc4b6977556f934aab815b22fb6bf6894000727850e5aa2e39d9e9825606280135c628f'
